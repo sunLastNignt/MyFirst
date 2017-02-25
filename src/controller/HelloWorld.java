a075980384db25ef39fdd0d37406a7a79d4a7edf
LastNight <910721696@qq.com>
2017-02-25 12:46:50 +0800

    Initial commit

---
+++ src/controller/HelloWorld.java
@@ -0,0 +1,10 @@
+package controller;
+
+/**
+ * Created by 91072 on 2017/2/21.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
